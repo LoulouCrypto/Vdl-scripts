fe4d16a2e5504db06edcfd572358bd11d62754d8
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-11-08 11:25:53 +0100

    Create erase.sh

---
+++ erase.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+cd /root/
+sleep 2
+rm -rf .vid*
+sleep 2
+rm /usr/local/bin/vid*
+sleep 2
+rm /etc/systemd/system/Vid*
+sleep 2
+rm vidulum-mn-install.sh
+ls .
+ls /usr/local/bin
+ls /etc/systemd/system/
