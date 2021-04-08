39dfeccc16981b6640e5700a25b3fda360205408
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-04-08 08:35:00 +0200

    Update vidulum-mn-install.sh

--- vidulum-mn-install.sh
+++ vidulum-mn-install.sh
@@ -11,7 +11,7 @@ CONFIGFOLDER='/root/.vidulum'
 PARAMFOLDER='/root/.vidulum-params'
 COIN_PATH='/usr/local/bin'
 #64 bit only
-COIN_TGZ='https://github.com/vidulum/vidulum/releases/download/v2.1.0/VDL-Linux.zip'
+COIN_TGZ='https://github.com/vidulum/vidulum/releases/download/v2.2.0/VDL-Linux.zip'
 BOOTSTRAP_TGZ='https://downloads.vidulum.app/bootstrap.zip'
 COIN_DAEMON="vidulumd"
 COIN_CLI="vidulum-cli"
