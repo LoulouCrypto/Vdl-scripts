a02d7fe6b9660da7c98bf5f3e59269ae5ab37086
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-06-14 22:14:18 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+cd ~
+sleep 1
+cd .vidulum
+sudo systemctl stop vidulum-$USER
+sleep 1
+vidulum-cli stop
+sleep 2
+rm -r blocks chainstate database
+sleep 2 
+wget https://downloads.vidulum.app/bootstrap.zip
+sleep 1 
+unzip bootstrap.zip
+sleep 1
+rm bootstrap.zip
+sleep 2
+sudo systemctl start vidulum-$USER
