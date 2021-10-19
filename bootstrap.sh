1cc5706e512a1549b758ab9fd02ba04195daaa0d
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-10-19 21:13:04 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,22 @@
 cd ~
 sleep 1
 cd .vidulum
+if [[ $USER == *root* ]]; 
+then
+systemctl stop Vidulum
+sleep 1
+vidulum-cli stop
+sleep 2
+rm -r blocks chainstate database
+sleep 2 
+wget https://zang.ovh/vdl/bootstrap.zip
+sleep 1 
+unzip bootstrap.zip
+sleep 1
+rm -r bootstrap*
+sleep 2
+systemctl start Vidulum
+else
 sudo systemctl stop vidulum-$USER
 sleep 1
 vidulum-cli stop
@@ -18,3 +34,4 @@ sleep 1
 rm -r bootstrap*
 sleep 2
 sudo systemctl start vidulum-$USER
+fi
